f19eba26ebd52ae56cd6b12331d8c268965ad04b
Nino <anthony@delcey.fr>
2023-01-10 15:52:44 +0100

    Add support for back press in DetailActivity

--- app/src/main/java/fr/delcey/openclassrooms_master_detail_mvvm/ui/detail/DetailActivity.kt
+++ app/src/main/java/fr/delcey/openclassrooms_master_detail_mvvm/ui/detail/DetailActivity.kt
@@ -1,6 +1,7 @@
 package fr.delcey.openclassrooms_master_detail_mvvm.ui.detail
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
 import fr.delcey.openclassrooms_master_detail_mvvm.databinding.DetailActivityBinding
@@ -13,10 +14,21 @@ class DetailActivity : AppCompatActivity() {
         val binding = DetailActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(binding.detailFlContainer.id, DetailFragment())
                 .commitNow()
         }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId) {
+        android.R.id.home -> {
+            supportFinishAfterTransition()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
